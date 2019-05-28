15e9dc1afaed70ac8528dbda9661fa0705629cb9
kioskadmin <kioskadmin@iscout18-8.bbhclan.org>
2019-05-28 17:03:56 +0000

    cloud-init and iscsi

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ sudo cp -r kiosk_user/. /home/kiosk
 sudo chown -R kiosk:kiosk /home/kiosk
 sudo chmod 500 /home/kiosk/.profile
 sudo chmod 500 /home/kiosk/.xinitrc
+echo 'datasource_list: [ None ]' | sudo -s tee /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+sudo apt-get purge cloud-init
+sudo rm -rf /etc/cloud/; sudo rm -rf /var/lib/cloud/
+sudo systemctl disable iscsid
+sudo systemctl disable open-iscsi
